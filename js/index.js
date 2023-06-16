cd3bef24fc65865ee614351103c81c2a572e1630
Whoozee <zaslavskyio@gmail.com>
2023-06-16 22:07:58 +0200

    fix code

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 function openNav() {
-	document.getElementById("slideMenu").style.width = "380px";
+  document.getElementById("slideMenu").style.width = "380px";
 }
 
 function closeNav() {
-	document.getElementById("slideMenu").style.width = "0";
+  document.getElementById("slideMenu").style.width = "0";
 }
